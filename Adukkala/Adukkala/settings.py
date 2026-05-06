18ebadc27ce20fb4983bb88ae30bc97d6d4d8333
AdithyaMC <mcadithya.9@gmail.com>
2026-05-06 19:39:34 +0530

    Add Malayalam language support: UTF-8 MySQL charset, Malayalam UI labels, timezone set to Asia/Kolkata

--- Adukkala/Adukkala/settings.py
+++ Adukkala/Adukkala/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', cast=bool, default=True)
 
 # Allow all hosts from environment or default
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1,adukkala.pythonanywhere.com').split(',')
+ALLOWED_HOSTS = [config('HOST_URL')]
 
 # Application definition
 INSTALLED_APPS = [
@@ -69,7 +69,11 @@ DATABASES = {
         'USER': config('DATABASE_USER'),
         'PASSWORD': config('DATABASE_PASSWORD'),
         'HOST': config('DATABASE_HOST'),
-        'PORT': config('DATABASE_PORT', cast=int, default=3306),
+        'PORT': '3306',
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
 
@@ -90,8 +94,8 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 # Internationalization
-LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'UTC'
+LANGUAGE_CODE = 'ml'
+TIME_ZONE = 'Asia/Kolkata'
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
